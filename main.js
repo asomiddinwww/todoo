a900565f5b9587a2b8fe0e0cbd84ee6d30d72971
Asomiddin <asomiddinto@gmail.com>
2025-10-26 14:56:37 +0500

    noww

--- main.js
+++ main.js
@@ -1,178 +1,3 @@
-// const arr = [123, 2, 3, 4, 5, 6, 237, 8, 9, 10]
-
-// function MinMax(arr) {
-//     let min = arr[0];
-//     let max = arr[0];
-
-//     for(let i = 0; i < arr.length; i++) {
-//         if(min > arr[i]) {
-//             min = arr[i];
-//         }
-//         if(max < arr.length) {
-//             max = arr[i];
-//         }
-//     }
-
-//     return `Eng kichik son: ${min}, Eng kotta son: ${max}`
-// }
-
-// console.log(MinMax(arr));
-
-
-
-
-
-
-
-
-// let arr = [1, 2, 33, 55, 66, 44, 88, 99, 4794, 51321, 2, 2, 0]
-
-// function MinMaxfuntion(arr) {
-//     let min = arr[0];
-//     let max = arr[0];
-
-//     for(i = 0; i < arr.length;i++){
-//         if(min > arr[i]) {
-//             min = arr[i]
-//         }
-//         if(max < arr[i]) {
-//             max = arr[i]
-//         }
-//     }
-
-//     return `kotta: ${min}, kichik: ${max}`
-
-// }
-
-// console.log(MinMaxfuntion(arr));
-
-// let num = 'molas';
-
-// let res = num.split('').reverse().join('');
-
-// console.log(res);
-
-
-
-// let sal = 'salom';
-// let res = ''
-
-// for(let i = sal.length - 1; i >= 0; i--){
-//     res += sal[i]
-// }
-// console.log(res);
-
-
-
-
-
-// let group ='salom';
-
-// let res = group.split('').reverse().join('');
-
-// console.log(
-// res
-// );
-
-
-
-
-
-// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-// function MinMax(arr) {
-//     let min = arr[0];
-//     let max = arr[0];
-
-//     for(i = 0;i < arr.length; i++) {
-//         if(min > arr[i]) {
-//             min = arr[i]
-//         }
-//         else if(max < arr[i]) {
-//             max = arr[i]
-//         }
-//     }
-//     return `Min: ${min}, Max: ${max}`
-// }
-
-// console.log(MinMax(arr));
-
-
-
-
-// let ism = 'Jahongir';
-
-// let res = '';
-
-// for(let i = ism.length - 1; i >= 0; i--) {
-//     res += ism[i]
-// }
-
-// console.log(res);
-
-// let btn = document.getElementById('btnn')
-// let img = document.getElementById('container')
-// let fuck = document.getElementById('fuck')
-
-// btn.addEventListener('click', (event) => {
-//     btn.remove();
-//     img.style.display = 'flex';
-//     fuck.remove();
-// })
-
-//     let count = 0;
-
-//     function increment() {
-//       count++;
-//       document.getElementById("counter").innerText = count;
-//     }
-
-//     function decrement() {
-//       count--;
-//       document.getElementById("counter").innerText = count;
-//     }
-
-// console.log(increment(55));
-
-
-// let num = 1;
-// let str = '11';
-
-// console.log(num + str);
-
-// let a = '10';
-// let b = '10.3';
-
-// console.log(Number(b));
-// console.log(Number.parseInt(b));
-// console.log(Number.apply(b));
-
-// console.log(true == true);
-
-
-// const content = document.querySelector('.content');
-// const send = document.querySelector('.active');
-// const btn = document.querySelector('.btn');
-
-// fetch("https://68fc61c596f6ff19b9f4f2dc.mockapi.io/ap1/users")
-//     .then((response) => response.json())
-//     .then((data) => { data.forEach((item) => {
-//        newDiv.innerHTML += `
-//         <h1>user: ${item.name}</h1>
-//         <p>age: ${item.age}</p>
-//         <hr>
-//     `}) 
-//     }) 
-//     .catch((error) => console.log(error))
-
-//     content.innerHTML = '';
-
-//     const newDiv = document.createElement('div')
-
-//     newDiv.className = 'person';
-//     content.prepend(newDiv);
-// btn.addEventListener('click', (event) => {
-// })
 
 
 const content = document.querySelector('.content');
@@ -181,7 +6,6 @@ const nameInput = document.querySelector('#name');
 const ageInput = document.querySelector('#age');
 const imgInput = document.querySelector('#img');
 
-// API-dan mavjud foydalanuvchilarni olish
 fetch("https://68fc61c596f6ff19b9f4f2dc.mockapi.io/ap1/users")
   .then((response) => response.json())
   .then((data) => {
@@ -190,7 +14,6 @@ fetch("https://68fc61c596f6ff19b9f4f2dc.mockapi.io/ap1/users")
   })
   .catch((error) => console.log(error));
 
-// DOMga foydalanuvchini qo‘shish funksiyasi
 function addUserToDOM(name, age, img, id) {
   const div = document.createElement('div');
   div.className = 'person';
@@ -210,7 +33,6 @@ function addUserToDOM(name, age, img, id) {
     </div>
   `;
 
-  // Delete
   div.querySelector('.delete').addEventListener('click', () => {
     const confirmDelete = confirm(`${name} ni o‘chirishni xohlaysizmi?`);
     if (!confirmDelete) return;
@@ -222,7 +44,6 @@ function addUserToDOM(name, age, img, id) {
       .catch(err => console.log(err));
   });
 
-  // Edit
   div.querySelector('.edit').addEventListener('click', () => {
     const newName = prompt("Yangi ism kiriting:", name);
     const newAge = prompt("Yangi yosh kiriting:", age);
@@ -246,7 +67,6 @@ function addUserToDOM(name, age, img, id) {
   content.prepend(div);
 }
 
-// Yangi foydalanuvchi qo‘shish
 btn.addEventListener('click', () => {
   const name = nameInput.value.trim();
   const age = ageInput.value.trim();
